810bec8811531fa927680156fd728262d7e02a16
doomersson <138079572+doomersson@users.noreply.github.com>
2023-08-27 22:58:14 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# TankiDesign
+A custom css design for tanki online fixing some of their bad design decisions
+
+# To install click [here](https://raw.githubusercontent.com/LordEliasTM/TankiDesign/main/design.user.js)
---
+++ design.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         TankiDesign
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  A custom css design for tanki online fixing some of their bad design decisions
+// @author       Tortuga
+
+// @match        *.tankionline.com/*
+// @run-at       document-start
+
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tankionline.com
+// @grant        none
+
+// @require      https://raw.githubusercontent.com/LordEliasTM/TankiDesign/main/design.prod.js
+// ==/UserScript==
+
+(()=>{})();
